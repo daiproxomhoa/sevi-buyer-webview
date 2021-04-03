3cc50d6c03b9d3defb0321e789bf2ba2ad8917ba
letuan1206 <tuanleanh1994@gmail.com>
2021-04-03 16:10:33 +0700

    Fix cardheader view on safari

--- src/modules/common/component/CardHeader.tsx
+++ src/modules/common/component/CardHeader.tsx
@@ -8,6 +8,7 @@ const CardHeader = (props: Props) => {
       style={{
         borderRadius: "0 0 32px 32px",
         height: "100%",
+        minHeight: "206px",
         background:
           "linear-gradient(96deg, #FFAE46 -31.54%, #FF9755 -2.71%, #CA1E78 59.37%, #C00FA7 98.43%)",
       }}
