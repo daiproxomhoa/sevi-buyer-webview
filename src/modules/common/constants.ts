01c691c109ce2b349b41dd4e0b65b4736b9caef9
Trung Ha <ktrungha@hotmail.com>
2021-04-06 20:40:28 +0700

    fix host

--- src/modules/common/constants.ts
+++ src/modules/common/constants.ts
@@ -3,7 +3,11 @@ export type some = { [key: string]: any };
 export const development: boolean =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
-export const APIHost = development ? "/api" : "https://production.com";
+export const APIHost = development
+  ? "/api"
+  : window.location.hostname.endsWith("freeginar.com")
+  ? "https://buyer.freeginar.com"
+  : "https://buyer.sevi.vn";
 
 export const TOKEN = "token";
 
