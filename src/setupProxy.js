23ade3173c83a02c0615491b1bc322a0978b34a1
Trung Ha <ktrungha@hotmail.com>
2021-04-08 11:35:35 +0700

    fix cookie setting domain issue

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,21 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const router = {
-  "/api": "https://buyer.freeginar.com/",
+  "/api": "https://buyer.freeginar.com",
 };
 
 module.exports = function (app) {
   app.use(
-    "/api/",
+    "/api",
     createProxyMiddleware({
       target: "https://buyer.freeginar.com",
       changeOrigin: true,
       secure: false,
       pathRewrite: {
-        "^/api": "/",
+        "^/api": "",
+      },
+      onProxyReq: function (proxyReq, req, res) {
+        proxyReq.removeHeader("Origin");
       },
       router,
       logLevel: "debug",
