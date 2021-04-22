c992c88e46f227cfffa014045b532a9a469cd377
letuan1206 <tuanleanh1994@gmail.com>
2021-04-22 16:45:46 +0700

    progressive image search result

--- src/modules/search/component/SearchResultItem.tsx
+++ src/modules/search/component/SearchResultItem.tsx
@@ -1,10 +1,11 @@
-import { Avatar, fade, ListItem, Typography } from "@material-ui/core";
-import GradeRoundedIcon from "@material-ui/icons/GradeRounded";
-import React from "react";
-import { FormattedMessage, FormattedNumber } from "react-intl";
-import { API_PATHS } from "../../../configs/api";
-import { LIGHT_GREY, PRIMARY, SECONDARY } from "../../../configs/colors";
-import { ISeller } from "../model";
+import { Avatar, fade, ListItem, Typography } from '@material-ui/core';
+import GradeRoundedIcon from '@material-ui/icons/GradeRounded';
+import React from 'react';
+import { FormattedMessage, FormattedNumber } from 'react-intl';
+import { API_PATHS } from '../../../configs/api';
+import { LIGHT_GREY, PRIMARY, SECONDARY } from '../../../configs/colors';
+import ProgressiveImage from '../../common/component/ProgressiveImage';
+import { ISeller } from '../model';
 
 interface Props {
   info: ISeller;
@@ -18,60 +19,50 @@ const SearchResultItem = (props: Props) => {
       button
       style={{
         backgroundColor: LIGHT_GREY,
-        display: "flex",
-        padding: "12px",
-        margin: "0px 0px 12px",
-        borderRadius: "12px",
-        alignItems: "flex-start",
+        display: 'flex',
+        padding: '12px',
+        margin: '0px 0px 12px',
+        borderRadius: '12px',
+        alignItems: 'flex-start',
       }}
       onClick={onSelect}
     >
-      <Avatar
+      <ProgressiveImage
         alt={info.givenName}
         src={API_PATHS.renderSellerAvatar(info.id, info.avatar)}
-        style={{ width: "40px", height: "40px" }}
+        style={{ width: '40px', height: '40px', borderRadius: '50%', objectFit: 'cover' }}
       />
-      <div
-        style={{ display: "flex", flexDirection: "column", marginLeft: "8px" }}
-      >
+      <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '8px' }}>
         <Typography variant="body2" style={{ fontWeight: 500 }}>
           {info.givenName}&nbsp;{info.familyName}
         </Typography>
 
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
           <div
             style={{
               backgroundColor: fade(PRIMARY, 0.1),
-              height: "20px",
-              minWidth: "52px",
-              borderRadius: "500px",
-              display: "flex",
-              alignItems: "center",
-              marginRight: "4px",
+              height: '20px',
+              minWidth: '52px',
+              borderRadius: '500px',
+              display: 'flex',
+              alignItems: 'center',
+              marginRight: '4px',
             }}
           >
             <GradeRoundedIcon
               color="primary"
               style={{
-                fontSize: "14px",
-                marginRight: "3px",
-                marginLeft: "8px",
+                fontSize: '14px',
+                marginRight: '3px',
+                marginLeft: '8px',
               }}
             />
-            <Typography
-              variant="body2"
-              color="primary"
-              style={{ marginRight: "8px" }}
-            >
+            <Typography variant="body2" color="primary" style={{ marginRight: '8px' }}>
               {info.rating}
             </Typography>
           </div>
           <Typography variant="caption" color="textSecondary">
-            <Typography
-              component="span"
-              variant="caption"
-              style={{ color: SECONDARY }}
-            >
+            <Typography component="span" variant="caption" style={{ color: SECONDARY }}>
               <FormattedNumber value={info.ratingCount} />
             </Typography>
             &nbsp;
