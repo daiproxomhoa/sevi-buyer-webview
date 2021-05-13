2f221cdcb623c69afe45d219c026eec17139fd4b
Trung Ha <ktrungha@hotmail.com>
2021-05-13 09:52:44 +0700

    default to logged in

--- src/modules/authen/redux/authenReducer.ts
+++ src/modules/authen/redux/authenReducer.ts
@@ -1,41 +1,30 @@
-import { Action } from "redux";
-import { ThunkAction } from "redux-thunk";
-import {
-  ActionType,
-  createAction,
-  createCustomAction,
-  getType,
-} from "typesafe-actions";
-import { createNonNullChain } from "typescript";
-import { API_PATHS } from "../../../configs/api";
-import { SUCCESS_CODE } from "../../../constants";
-import { AppState } from "../../../redux/reducer";
-import { setLoadingBackDrop } from "../../common/redux/commonReducer";
-import { fetchThunk } from "../../common/redux/thunk";
-import { IAuth } from "../model";
+import { Action } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { ActionType, createAction, createCustomAction, getType } from 'typesafe-actions';
+import { API_PATHS } from '../../../configs/api';
+import { SUCCESS_CODE } from '../../../constants';
+import { AppState } from '../../../redux/reducer';
+import { setLoadingBackDrop } from '../../common/redux/commonReducer';
+import { fetchThunk } from '../../common/redux/thunk';
+import { IAuth } from '../model';
 
 export interface AuthenState {
   authen: boolean;
   data?: IAuth;
 }
 
-export const authenIn = createCustomAction("authen/in");
+export const authenIn = createCustomAction('authen/in');
 
-export const authenOut = createCustomAction("authen/out");
+export const authenOut = createCustomAction('authen/out');
 
-export const setAuthData = createAction("auth/setAuthData", (data: IAuth) => ({
+export const setAuthData = createAction('auth/setAuthData', (data: IAuth) => ({
   data,
 }))();
 
-export function logout(): ThunkAction<
-  Promise<void>,
-  AppState,
-  null,
-  Action<string>
-> {
+export function logout(): ThunkAction<Promise<void>, AppState, null, Action<string>> {
   return async (dispatch) => {
     dispatch(setLoadingBackDrop(true));
-    const json = await dispatch(fetchThunk(API_PATHS.signOut, "get"));
+    const json = await dispatch(fetchThunk(API_PATHS.signOut, 'get'));
     if (json.status === SUCCESS_CODE) {
       dispatch(authenOut());
     }
@@ -51,10 +40,7 @@ const actions = {
 
 type ActionT = ActionType<typeof actions>;
 
-export default function authenReducer(
-  state = { authen: false, user: createNonNullChain },
-  action: ActionT
-): AuthenState {
+export default function authenReducer(state = { authen: true }, action: ActionT): AuthenState {
   switch (action.type) {
     case getType(authenIn):
       return { ...state, authen: true };
